e8f4638b0ceda8825cfd146d9040e1f704ca4217
Anton Zekeriev Rodin <antonzekeriev@gmail.com>
2018-04-25 14:23:16 +0200

    Add Basic Toggle Module

--- toggle/README.md
+++ toggle/README.md
@@ -3,3 +3,11 @@
 ```javascript
     elem.style.display = (elem.style.display == "none") ? "block" : "none";
 ```
+
+## Toggle Class
+
+Add listener to toggle content. Shold give button and content CSS Selectos
+
+```javascript
+myButton = new Toggle('#toggle-button-2', '.toggle-content-2');
+```
--- toggle/app.js
+++ toggle/app.js
@@ -8,3 +8,46 @@ btn.addEventListener('click', e => {
 function toggle(elem) {
     elem.style.display = (elem.style.display == "none") ? "block" : "none";
 }
+
+
+/**
+ * Button, event Handler ES5
+ */
+
+Toggle = function(button, content) {
+
+    const elem = document.querySelector(content);
+    const btn = document.querySelector(button);
+
+    btn.addEventListener('click', e => {
+        elem.style.display = (elem.style.display == "none") ? "block" : "none";
+    });
+
+
+    return {
+        btn,
+        elem,
+        hide: function() {
+            elem.style.display = 'none';
+        },
+        show: function() {
+            elem.style.display = 'block';
+        }
+    }
+
+ };
+
+myButton = new Toggle('#toggle-button-2', '.toggle-content-2');
+myButton2 = new Toggle('#toggle-button-3', '.toggle-content-3');
+
+/*
+setInterval(function() {
+    myButton.hide();
+    console.log('Hide Element in 3 seg')
+}, 3000);
+
+setInterval(function() {
+    myButton.show();
+    console.log('Show Element in 3 seg')
+}, 3000);
+*/
